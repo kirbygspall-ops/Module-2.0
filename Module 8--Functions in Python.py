da847eba6904df651a9ecddbd559bc8bdae9d3a2
Kirby Spall <kirbygspall@gmail.com>
2026-05-06 18:38:45 -0400

    Add files via upload

---
+++ Module 8--Functions in Python.py
@@ -0,0 +1,12 @@
+def greater_than(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+a = 10
+b = 6
+
+result = greater_than(a, b)
+
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
